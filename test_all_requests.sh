9888554d5aeb5dcf69b7e2db8671deffefc57ee9
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2015-06-01 15:49:09 -0300

    Fix bug: Script parameters updated

--- test_all_requests.sh
+++ test_all_requests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 source rest.cfg
 
-TOKEN=$(./login.sh ${EMAIL} ${PASSWORD})
+TOKEN=$(./login.sh ${DSPACEURL} ${EMAIL} ${PASSWORD})
 
 for R in $(ls requests/POST_*); do
   for T in json xml; do
     echo "EXECUTING REQUEST: $R ($T):"
     echo "--------------------------------------------"
-    ./send_request.sh $DSPACEURL $TOKEN $R $T
+    ./send_request.sh ${DSPACEURL} ${TOKEN} $R $T
     echo ""
     echo "--------------------------------------------"
     echo ""
@@ -18,7 +18,7 @@ for R in $(ls requests/PUT_*); do
   for T in json xml; do
     echo "EXECUTING REQUEST: $R ($T):"
     echo "--------------------------------------------"
-    ./send_request.sh $DSPACEURL $TOKEN $R $T
+    ./send_request.sh ${DSPACEURL} ${TOKEN} $R $T
     echo ""
     echo "--------------------------------------------"
     echo ""
@@ -29,11 +29,11 @@ for R in $(ls requests/UPLOAD_*); do
   for T in json xml; do
     echo "EXECUTING REQUEST: $R ($T):"
     echo "--------------------------------------------"
-    ./send_file.sh $DSPACEURL $TOKEN $R $T
+    ./send_file.sh ${DSPACEURL} ${TOKEN} $R $T
     echo ""
     echo "--------------------------------------------"
     echo ""
   done
 done
 
-./logout.sh ${TOKEN}
+./logout.sh ${DSPACEURL} ${TOKEN}
